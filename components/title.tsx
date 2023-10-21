400186571452b77e67c6e943eba358de3ceb92f7
swajp <stejskalmira@icloud.com>
2023-10-22 00:45:05 +0200

    title text

--- components/title.tsx
+++ components/title.tsx
@@ -3,12 +3,13 @@ import React from "react";
 export default function Title() {
   return (
     <div className="h-[20vh]">
-      <div className="flex flex-col text-center mt-20 md:mt-24 gap-2">
+      <div className="flex flex-col text-center items-center  mt-20 md:mt-24 gap-4">
         <h1 className=" text-5xl md:text-6xl font-bold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-gray-400  to-gray-900">
           Hi, I'm Miroslav.
         </h1>
-        <p className=" text-sm md:text-base text-gray-500 tracking-wide">
-          I am FullStack Developer. I develop.
+        <p className=" text-sm md:text-base text-gray-500 tracking-wide max-w-lg">
+          I love to create catchy and modern websites. I am a self-taught web
+          developer and I am always looking for new challenges.
         </p>
       </div>
     </div>
