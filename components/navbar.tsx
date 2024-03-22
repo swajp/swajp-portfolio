7e1afc4c92e9551f31e9eb383fa9da28e6061073
swajp <stejskalmira@icloud.com>
2024-03-22 21:17:55 +0100

    better ux, add contact

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
 
@@ -47,6 +47,26 @@ const routes = [
       </svg>
     ),
   },
+  {
+    label: "Contact",
+    href: "/contact",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+        />
+      </svg>
+    ),
+  },
 ];
 
 const variants = {
@@ -57,6 +77,31 @@ const variants = {
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
   const pathname = usePathname();
+
+  const navbarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target as Node)
+      ) {
+        setNavbar(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Escape") {
+      setNavbar(false);
+    }
+  };
+
   return (
     <header className="transparent md:sticky top-0 z-30 ">
       <nav className="">
@@ -76,7 +121,7 @@ export default function Navbar() {
               />
             </motion.div>
           </Link>
-          <div className="relative">
+          <div className="relative" ref={navbarRef} onKeyDown={handleKeyDown}>
             <motion.button
               whileHover={{ scale: 0.9 }}
               whileTap={{ scale: 1.1 }}
@@ -139,7 +184,7 @@ export default function Navbar() {
                     href={route.href}
                     onClick={() => setNavbar(false)}
                   >
-                    <div className=" pr-24 pl-2 py-4 rounded-2xl bg-white hover:bg-gray-50 flex gap-2 ">
+                    <div className=" pr-24 pl-2 py-4 rounded-2xl bg-white hover:bg-gray-50 flex gap-3 ">
                       {route.svg}
                       <p className="text-gray-500 font-medium ">
                         {route.label}
